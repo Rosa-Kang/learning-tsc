1a344e490ace868de81a8cc11e80d46e670feca1
Rosa Kang <therosessom@gmail.com>
2023-06-29 09:30:44 -0700

    update oop

--- 3-oop/3-1-without-oop.ts
+++ 3-oop/3-1-without-oop.ts
@@ -1,3 +1,13 @@
+/**
+ * Imperative & Procedural Programming 절차지향적 프그래밍
+ * 단점: 하나를 수정하기 위해서는 전체를 이해해야 함.
+ * 
+ * OOP
+ * 징점 : 한곳에서 문제가 생기면 관련된 오브젝트만 이해하면 됨. reusability, productivity, faster 
+ * Error, Exception, Event 도 Object 로 구현할 수 있음.
+ */
+
+
 {
   type CoffeeCup = {
     shots: number;
